3d6c5dce50e70cc9067d93e0472b3d77063de0c1
oeksuez <oeksuezemre@outlook.de>
2023-12-06 10:20:21 +0100

    slochecker efficiency test

--- slo-checker/efficiency/app/main.py
+++ slo-checker/efficiency/app/main.py
@@ -5,6 +5,7 @@ import json
 import sys
 import re
 import pandas as pd
+import csv
 
 
 app = FastAPI()
@@ -65,6 +66,48 @@ def check_result(result, operator: str, threshold):
         return False # Mainly used for testing
     else:
         raise ValueError('Invalid operator string.')
+    
+
+
+
+def handle_query(data):
+    print("call if workloadQuery provided, thus ")
+
+
+def handle_efficiency(data):
+    load = data[1]
+    results = data[0]
+
+    for r in results:
+        # potentially base 
+        first_pair = r['first'] 
+        first_string = str(first_pair['first'])
+        first_first_listOfProms = first_pair['second']
+        first_second_listOfProms = first_pair['third']
+
+
+        second_pair = r['second'] 
+        second_string = str(second_pair['first'])
+        second_first_listOfProms = second_pair['second']
+        second_second_listOfProms = second_pair['third']
+
+
+        # if(len(first_first_listOfProms) < 1):
+        #     query_results = [aggr_query(r[0]["values"], warmup, query_aggregation) for r in data["results"]]
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -72,6 +115,18 @@ def check_result(result, operator: str, threshold):
 async def check_slo(request: Request):
     data = json.loads(await request.body())
     logger.info('Received request with metadata: %s', data['metadata'])
+    # Open a file for writing
+    with open('output.csv', 'w', newline='') as file:
+        # Create a CSV writer
+        writer = csv.writer(file)
+
+        # Write the header (keys of the first dictionary in the list)
+        writer.writerow(data[0].keys())
+
+        # Write the data rows
+        for item in data:
+            writer.writerow(item.values())
+
 
     warmup = int(data['metadata']['warmup'])
     query_aggregation = get_aggr_func(data['metadata']['queryAggregation'])
@@ -79,8 +134,16 @@ async def check_slo(request: Request):
     operator = data['metadata']['operator']
     threshold = float(data['metadata']['threshold'])
 
-    query_results = [aggr_query(r[0]["values"], warmup, query_aggregation) for r in data["results"]]
-    result = pd.DataFrame(query_results).aggregate(rep_aggregation).at[0]
-    return check_result(result, operator, threshold)
+    return True
+    # isWithQuery = len(data["results"]) > 1
+
+    # if isWithQuery: 
+    #     handle_efficiency(data["results"])
+    # else:
+    #     handle_query(data["results"])
+
+    # query_results = [aggr_query(r[0]["values"], warmup, query_aggregation) for r in data["results"]]
+    # result = pd.DataFrame(query_results).aggregate(rep_aggregation).at[0]
+    # return check_result(result, operator, threshold)
 
 logger.info("SLO evaluator is online")
